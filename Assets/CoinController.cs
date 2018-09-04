7748e726451449a0750b59edafd89984360ba2ed
yamazaki0305 <yamazaki0305@hotmail.com>
2018-09-05 01:39:22 +0900

    初めてのコミット

---
+++ Assets/CoinController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CoinController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        // 回転を開始する角度を設定
+        this.transform.Rotate(0, UnityEngine.Random.Range(0, 360), 0);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //回転
+        this.transform.Rotate(0, 3, 0);
+
+    }
+}
---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MyCameraController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+}
